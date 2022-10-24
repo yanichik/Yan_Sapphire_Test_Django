4c656586ea4090777e9b458d144c2dc659ab681f
yan-brunshteyn <yan@thecameoco.com>
2022-10-24 13:38:41 -0700

    submission

--- pages/views.py
+++ pages/views.py
@@ -1,11 +1,16 @@
 from django.shortcuts import render
 
+from django.contrib.auth import authenticate
+
 from submissions.models import Submission
 
 
 def home(request):
     context = {}
+    # print("authenticate", authenticate)
+    myUser = authenticate(username="yanichik", password="russia20")
+    print("myUser", myUser)
 
-    context['submissions'] = Submission.objects.all().order_by('-rating')[:5]
+    context["submissions"] = Submission.objects.all().order_by("-rating")[:10]
 
-    return render(request, 'pages/home.html', context)
+    return render(request, "pages/home.html", context)
